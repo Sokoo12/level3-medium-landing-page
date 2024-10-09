021b19f3156cfb142a9d015679d12b2af6238de6
Solongo <sssokoo128@gmail.com>
2024-10-09 19:50:50 +0800

    first

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,149 @@
-import Image from "next/image";
 
-export default function Home() {
+
+const Desktop = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <div className="w-full relative bg-white flex flex-col items-start justify-start text-center text-sm text-black font-inter">
+          <div className="self-stretch h-[900px] flex flex-col items-start justify-start">
+              <div className="self-stretch flex-1 bg-seashell overflow-hidden flex flex-col items-start justify-start min-h-[560px] z-[1]">
+                  <div className="self-stretch border-gray-100 border-b-[1px] border-solid flex flex-row items-center justify-center py-3.5 px-[124px]">
+                      <div className="flex-1 flex flex-row items-center justify-between">
+                          <img className="w-[112.3px] relative h-[25px]" alt="" src="Vector.svg" />
+                          <div className="flex flex-row items-center justify-start gap-[74px]">
+                              <div className="flex flex-row items-center justify-start gap-[29px]">
+                                  <div className="w-[65px] relative leading-[18px] flex items-center justify-center h-[19px] shrink-0">Our story</div>
+                                  <div className="w-[84px] relative leading-[18px] flex items-center justify-center h-[19px] shrink-0">Membership</div>
+                                  <div className="w-[38px] relative leading-[18px] flex items-center justify-center h-[19px] shrink-0">Write</div>
+                                  <div className="w-[51px] relative leading-[18px] flex items-center justify-center h-[19px] shrink-0">Sign in</div>
+                              </div>
+                              <div className="w-[103.7px] rounded-1961xl bg-gray-200 border-gray-200 border-[1px] border-solid box-border h-[37.6px] flex flex-row items-start justify-center py-[9px] px-[40.2px] text-white">
+                                  <div className="relative leading-[18px] font-semibold">Get started</div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="self-stretch flex-1 overflow-hidden flex flex-row items-center justify-center pt-[233.6px] px-[124px] pb-[158.6px] relative text-left text-[111.91px] text-gray-100">
+                      <div className="flex-1 flex flex-col items-start justify-start gap-12 max-w-[1192px] z-[0]">
+                          <div className="self-stretch flex flex-col items-start justify-start">
+                              <div className="self-stretch relative tracking-[-6.6px] leading-[100px]">
+                                  <p className="m-0">Human</p>
+                                  <p className="m-0">{`stories & ideas`}</p>
+                              </div>
+                          </div>
+                          <div className="self-stretch flex flex-col items-start justify-start text-[20.45px]">
+                              <div className="self-stretch relative leading-[28px]">A place to read, write, and deepen your understanding</div>
+                          </div>
+                          <div className="self-stretch flex flex-col items-start justify-start text-center text-[18.75px] text-white">
+                              <div className="w-[196px] rounded-1961xl bg-gray-200 border-gray-200 border-[1px] border-solid box-border flex flex-row items-start justify-start py-[9px] px-[40.2px]">
+                                  <div className="relative leading-[28px]">Start reading</div>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="w-[460px] !m-[0] absolute top-[111.58px] right-[0px] flex flex-col items-start justify-center z-[1]">
+                          <img className="w-[460px] relative h-[600px] overflow-hidden shrink-0 object-cover" alt="" src="/src/app/back.png" />
+                      </div>
+                  </div>
+              </div>
+              <div className="self-stretch bg-seashell flex flex-col items-start justify-start z-[0] text-left text-smi-7 text-dimgray">
+                  <div className="self-stretch flex flex-col items-start justify-start">
+                      <div className="self-stretch relative border-gray-100 border-b-[1px] border-solid box-border h-px" />
+                      <div className="self-stretch flex flex-row items-start justify-center py-0 px-[380px]">
+                          <div className="self-stretch flex-1 flex flex-col items-start justify-start max-w-[680px]">
+                              <div className="self-stretch flex flex-row items-start justify-start flex-wrap content-start py-6 px-0 gap-0">
+                                  <div className="self-stretch flex flex-col items-start justify-center py-0 pl-0 pr-4">
+                                      <div className="h-5 flex flex-col items-start justify-start">
+                                          <div className="self-stretch flex flex-row items-start justify-center">
+                                              <div className="flex-1 flex flex-col items-start justify-start">
+                                                  <div className="relative leading-[20px]">Help</div>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div className="self-stretch flex flex-col items-start justify-center py-0 pl-0 pr-4 text-[12.19px]">
+                                      <div className="h-5 flex flex-col items-start justify-start">
+                                          <div className="self-stretch flex flex-row items-start justify-center">
+                                              <div className="flex-1 flex flex-col items-start justify-start">
+                                                  <div className="relative leading-[20px]">Status</div>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div className="self-stretch flex flex-col items-start justify-center py-0 pl-0 pr-4 text-[12.59px]">
+                                      <div className="h-5 flex flex-col items-start justify-start">
+                                          <div className="self-stretch flex flex-row items-start justify-center">
+                                              <div className="flex-1 flex flex-col items-start justify-start">
+                                                  <div className="relative leading-[20px]">About</div>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div className="self-stretch flex flex-col items-start justify-center py-0 pl-0 pr-4 text-[12.19px]">
+                                      <div className="h-5 flex flex-col items-start justify-start">
+                                          <div className="self-stretch flex flex-row items-start justify-center">
+                                              <div className="flex-1 flex flex-col items-start justify-start">
+                                                  <div className="relative leading-[20px]">Careers</div>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div className="self-stretch flex flex-col items-start justify-center py-0 pl-0 pr-4 text-[12.39px]">
+                                      <div className="h-5 flex flex-col items-start justify-start">
+                                          <div className="self-stretch flex flex-row items-start justify-center">
+                                              <div className="flex-1 flex flex-col items-start justify-start">
+                                                  <div className="relative leading-[20px]">Press</div>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div className="self-stretch flex flex-col items-start justify-center py-0 pl-0 pr-4">
+                                      <div className="h-5 flex flex-col items-start justify-start">
+                                          <div className="self-stretch flex flex-row items-start justify-center">
+                                              <div className="flex-1 flex flex-col items-start justify-start">
+                                                  <div className="relative leading-[20px]">Blog</div>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div className="self-stretch flex flex-col items-start justify-center py-0 pl-0 pr-4 text-[12.29px]">
+                                      <div className="h-5 flex flex-col items-start justify-start">
+                                          <div className="self-stretch flex flex-row items-start justify-center">
+                                              <div className="flex-1 flex flex-col items-start justify-start">
+                                                  <div className="relative leading-[20px]">Privacy</div>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div className="self-stretch flex flex-col items-start justify-center py-0 pl-0 pr-4 text-[12.39px]">
+                                      <div className="h-5 flex flex-col items-start justify-start">
+                                          <div className="self-stretch flex flex-row items-start justify-center">
+                                              <div className="flex-1 flex flex-col items-start justify-start">
+                                                  <div className="relative leading-[20px]">Terms</div>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div className="self-stretch flex flex-col items-start justify-center py-0 pl-0 pr-4 text-[11.98px]">
+                                      <div className="h-5 flex flex-col items-start justify-start">
+                                          <div className="self-stretch flex flex-row items-start justify-center">
+                                              <div className="flex-1 flex flex-col items-start justify-start">
+                                                  <div className="relative leading-[20px]">Text to speech</div>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div className="self-stretch flex flex-col items-start justify-start text-[12.29px]">
+                                      <div className="self-stretch flex flex-row items-start justify-center">
+                                          <div className="flex-1 flex flex-col items-start justify-start">
+                                              <div className="relative leading-[20px]">Teams</div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>);
+};
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+export default Desktop;
